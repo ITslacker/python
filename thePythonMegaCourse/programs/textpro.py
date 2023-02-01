9a375f723108fa465f7fa00fd5a58f0c1875c23a
Steven Hoong <Shoong@getty.edu>
2023-02-01 08:30:37 -0800

    completed more lessons

---
+++ thePythonMegaCourse/programs/textpro.py
@@ -0,0 +1,16 @@
+def sentence_maker(phrase):
+  capitalized = phrase.capitalize()
+  if phrase.startswith(("how", "what", "why")):
+    return "{}?".format(capitalized)
+  else:
+    return "{}.".format(capitalized)
+
+results = []
+while True:
+  user_input = input("Say something: ")
+  if user_input == "\end":
+    break
+  else:
+    results.append(sentence_maker(user_input))
+
+print(" ".join(results))
