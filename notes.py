81e0e1912327c6741036fbe75fb84cccbd790a3b
Steven Hoong <Shoong@getty.edu>
2023-02-01 08:39:35 -0800

    added note on how to find methods of builtin functions

--- notes.py
+++ notes.py
@@ -1,6 +1,6 @@
 # helpful commands
 
-dir(__builtins__) # this will list builin functions you can use
+dir(__builtins__) # this will list builin functions you can use, you can then use dir($builtinfunction) to find the methods
 help() # python equivelent to man page, useful to find builtin mutables of a builtin function, ex. help(list) help(dict)
 
 # useful commands
